f1d4f51ab0c1d7297a7ebf1415cba15a18ac85d3
l bates childs <labchilds@gmail.com>
2021-12-18 17:59:55 -0500

    full fetch function added

--- assets/script/homepage.js
+++ assets/script/homepage.js
@@ -1,5 +1,13 @@
-function getUserRepos() {
-    fetch('https://api.github.com/users/octocat/repos');
+function getUserRepos(user) {
+    // format api url with user input data
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+    // make request to url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data) {
+            console.log(data)
+        });
+    });
 };
 
 getUserRepos();
